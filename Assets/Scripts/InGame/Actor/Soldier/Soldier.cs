6b4d7904dce3dd2a2671a2a9574333b8f684f43c
yumehiko <yumehik0@icloud.com>
2021-03-27 19:27:33 +0900

    Spineライブラリ

---
+++ Assets/Scripts/InGame/Actor/Soldier/Soldier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 兵士。プレイヤーを捕獲しようと接近するActor。
+/// </summary>
+public class Soldier : MonoBehaviour
+{
+    private TurnAct turnAct = default;
+    public TurnAct TurnAct => turnAct;
+
+    private Movement movement = default;
+    public Movement Movement => movement;
+
+    private void Awake()
+    {
+        turnAct = new TurnAct();
+        movement = new Movement(turnAct, transform);
+    }
+}
---
+++ Assets/Scripts/InGame/SeeTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+/// <summary>
+/// 邪眼でみられたとき、反応する機構。
+/// </summary>
+public class SeeTarget : MonoBehaviour
+{
+    private Subject<bool> seeing = new Subject<bool>();
+    public System.IObservable<bool> Seeing => seeing;
+
+    private void Awake()
+    {
+        GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+}
