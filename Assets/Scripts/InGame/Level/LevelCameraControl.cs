6a9e2d5ddde1e3cd64cbefbed165571fa803d61e
yumehiko <yumehik0@icloud.com>
2022-07-27 13:05:57 +0900

    GoalPrefabの調整

---
+++ Assets/Scripts/InGame/Level/LevelCameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LevelCameraControl : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera levelCamera = default;
+    public CinemachineVirtualCamera LevelCamera => levelCamera;
+
+    /// <summary>
+    /// カメラをシェイクする。
+    /// </summary>
+    public void ShakeCamera()
+    {
+
+    }
+}
